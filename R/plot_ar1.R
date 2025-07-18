9724c306244644aef5ad39f4e7d5f46932ee6acb
filipe-manoel <filipe.manoel@ufv.br>
2025-07-18 10:18:42 -0300

    changing colors

--- R/plot_ar1.R
+++ R/plot_ar1.R
@@ -10,40 +10,41 @@
 #' @export
 #'
 #' @examples plot_ar1(n=5, rho = 0.7, save_path = "plot.png", width = 8, height = 8, dpi = 300)
-
+#'
 #'
 plot_ar1 = function(n = 5, rho = 0.7, save_path = NULL, width = 5, height = 5, dpi = 300) {
 
   # Estética padrão
-  base_cartoon_plot = function(df, title, gray_zero = FALSE) {
-    palette_colors = c("#f4a582", "#92c5de", "#d5d5d5", "#a6d854", "#ffd92f", "#e78ac3")
 
-    if (gray_zero) {
-      df$value_plot = ifelse(df$value == 0, NA, df$value)
-      p = ggplot2::ggplot(df, ggplot2::aes(Var1, Var2, fill = value_plot)) +
-        ggfx::with_shadow(ggplot2::geom_tile(color = "black", size = 1), sigma = 5, x_offset = 2, y_offset = 2, colour = "grey50") +
-        ggplot2::scale_fill_gradientn(colors = palette_colors, na.value = "grey90") +
-        ggplot2::guides(fill = "none")
-    } else {
-      p = ggplot2::ggplot(df, ggplot2::aes(Var1, Var2, fill = value)) +
-        ggfx::with_shadow(ggplot2::geom_tile(color = "black", size = 1), sigma = 5, x_offset = 2, y_offset = 2, colour = "grey50") +
-        ggplot2::scale_fill_gradientn(colors = palette_colors) +
-        ggplot2::guides(fill = "none")
-    }
 
-    p +
+  base_cartoon_plot <- function(df, title) {
+    df$fill_val <- ifelse(df$value == 0, NA, df$value)
+
+    ggplot2::ggplot(df, aes(Var1, Var2, fill = fill_val)) +
+      ggfx::with_shadow(
+        ggplot2::geom_tile(color = "black", size = 1),
+        sigma = 5, x_offset = 2, y_offset = 2, colour = "grey50"
+      ) +
+      ggplot2::scale_fill_distiller(
+        palette  = "YlOrRd",   # qualquer paleta contínua do RColorBrewer
+        direction= 1,
+        na.value = "grey90"
+      ) +
+      ggplot2::guides(fill = "none") +
       ggplot2::theme_void() +
       ggplot2::ggtitle(title) +
       ggplot2::theme(
-        plot.title = ggplot2::element_text(family = "sans", size = 13, hjust = 0.5, face = "bold"),
-        plot.background = ggplot2::element_rect(fill = "transparent", color = NA)
+        plot.title      = ggplot2::element_text(family="sans", face="bold", hjust=0.5),
+        plot.background = ggplot2::element_rect(fill="transparent", color=NA)
       ) +
       ggplot2::coord_fixed() +
       ggplot2::scale_y_reverse()
   }
 
-  # Definition of the structure
 
+
+
+  # Definition of the structure
   mat = toeplitz(rho^(0:(n-1)))
   df = reshape2::melt(mat)
   p = base_cartoon_plot(df, paste0("AR1 (2 params)"))
@@ -55,5 +56,9 @@ plot_ar1 = function(n = 5, rho = 0.7, save_path = NULL, width = 5, height = 5, d
   }
 
   print(p)
+  print(as.matrix(mat))
 
 }
+
+plot_ar1()
+
